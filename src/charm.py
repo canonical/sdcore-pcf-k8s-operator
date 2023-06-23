c7c180d4e9070adc4fb91c15c1c574837f6e89b6
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-06-23 06:35:59 -0400

    [Feat] Implementation of the TLS interface (#10)

--- src/charm.py
+++ src/charm.py
@@ -14,6 +14,13 @@ from charms.observability_libs.v1.kubernetes_service_patch import (  # type: ign
     KubernetesServicePatch,
 )
 from charms.sdcore_nrf.v0.fiveg_nrf import NRFRequires  # type: ignore[import]
+from charms.tls_certificates_interface.v2.tls_certificates import (  # type: ignore[import]
+    CertificateAvailableEvent,
+    CertificateExpiringEvent,
+    TLSCertificatesRequiresV2,
+    generate_csr,
+    generate_private_key,
+)
 from jinja2 import Environment, FileSystemLoader
 from lightkube.models.core_v1 import ServicePort
 from ops.charm import CharmBase
@@ -30,6 +37,11 @@ PCF_SBI_PORT = 29507
 DATABASE_NAME = "free5gc"
 DATABASE_RELATION_NAME = "database"
 NRF_RELATION_NAME = "fiveg_nrf"
+CERTS_DIR_PATH = "/support/TLS"  # Certificate paths are hardcoded in PCF code
+PRIVATE_KEY_NAME = "pcf.key"
+CSR_NAME = "pcf.csr"
+CERTIFICATE_NAME = "pcf.pem"
+CERTIFICATE_COMMON_NAME = "pcf.sdcore"
 
 
 class PCFOperatorCharm(CharmBase):
@@ -37,6 +49,8 @@ class PCFOperatorCharm(CharmBase):
 
     def __init__(self, *args):
         super().__init__(*args)
+        if not self.unit.is_leader():
+            raise NotImplementedError("Scaling is not implemented for this charm")
         self._container_name = self._service_name = "pcf"
         self._container = self.unit.get_container(self._container_name)
 
@@ -44,18 +58,30 @@ class PCFOperatorCharm(CharmBase):
             self, relation_name="database", database_name=DATABASE_NAME
         )
         self._nrf_requires = NRFRequires(charm=self, relation_name=NRF_RELATION_NAME)
-
-        self.framework.observe(self.on.database_relation_joined, self._configure_sdcore_pcf)
-        self.framework.observe(self._database.on.database_created, self._configure_sdcore_pcf)
-
-        self.framework.observe(self.on.fiveg_nrf_relation_joined, self._configure_sdcore_pcf)
-        self.framework.observe(self._nrf_requires.on.nrf_available, self._configure_sdcore_pcf)
-
-        self.framework.observe(self.on.pcf_pebble_ready, self._configure_sdcore_pcf)
-
         self._service_patcher = KubernetesServicePatch(
             charm=self,
             ports=[ServicePort(name="sbi", port=PCF_SBI_PORT)],
+        )
+        self._certificates = TLSCertificatesRequiresV2(self, "certificates")
+        self.framework.observe(self.on.database_relation_joined, self._configure_sdcore_pcf)
+        self.framework.observe(self._database.on.database_created, self._configure_sdcore_pcf)
+        self.framework.observe(self.on.fiveg_nrf_relation_joined, self._configure_sdcore_pcf)
+        self.framework.observe(self._nrf_requires.on.nrf_available, self._configure_sdcore_pcf)
+        self.framework.observe(self.on.pcf_pebble_ready, self._configure_sdcore_pcf)
+        self.framework.observe(
+            self.on.certificates_relation_created, self._on_certificates_relation_created
+        )
+        self.framework.observe(
+            self.on.certificates_relation_joined, self._on_certificates_relation_joined
+        )
+        self.framework.observe(
+            self.on.certificates_relation_broken, self._on_certificates_relation_broken
+        )
+        self.framework.observe(
+            self._certificates.on.certificate_available, self._on_certificate_available
+        )
+        self.framework.observe(
+            self._certificates.on.certificate_expiring, self._on_certificate_expiring
         )
 
     def _configure_sdcore_pcf(self, event: EventBase):
@@ -93,6 +119,138 @@ class PCFOperatorCharm(CharmBase):
         self._configure_pebble(restart=restart)
         self.unit.status = ActiveStatus()
 
+    def _on_certificates_relation_created(self, event: EventBase) -> None:
+        """Generates Private key."""
+        if not self._container.can_connect():
+            event.defer()
+            return
+        self._generate_private_key()
+
+    def _on_certificates_relation_broken(self, event: EventBase) -> None:
+        """Deletes TLS related artifacts and reconfigures workload."""
+        if not self._container.can_connect():
+            event.defer()
+            return
+        self._delete_private_key()
+        self._delete_csr()
+        self._delete_certificate()
+        self._configure_sdcore_pcf(event)
+
+    def _on_certificates_relation_joined(self, event: EventBase) -> None:
+        """Generates CSR and requests new certificate."""
+        if not self._container.can_connect():
+            event.defer()
+            return
+        if not self._private_key_is_stored():
+            event.defer()
+            return
+        self._request_new_certificate()
+
+    def _on_certificate_available(self, event: CertificateAvailableEvent) -> None:
+        """Pushes certificate to workload and configures workload."""
+        if not self._container.can_connect():
+            event.defer()
+            return
+        if not self._csr_is_stored():
+            logger.warning("Certificate is available but no CSR is stored")
+            return
+        if event.certificate_signing_request != self._get_stored_csr():
+            logger.debug("Stored CSR doesn't match one in certificate available event")
+            return
+        self._store_certificate(event.certificate)
+        self._configure_sdcore_pcf(event)
+
+    def _on_certificate_expiring(self, event: CertificateExpiringEvent) -> None:
+        """Requests new certificate."""
+        if not self._container.can_connect():
+            event.defer()
+            return
+        if event.certificate != self._get_stored_certificate():
+            logger.debug("Expiring certificate is not the one stored")
+            return
+        self._request_new_certificate()
+
+    def _generate_private_key(self) -> None:
+        """Generates and stores private key."""
+        private_key = generate_private_key()
+        self._store_private_key(private_key)
+
+    def _request_new_certificate(self) -> None:
+        """Generates and stores CSR, and uses it to request a new certificate."""
+        private_key = self._get_stored_private_key()
+        csr = generate_csr(
+            private_key=private_key,
+            subject=CERTIFICATE_COMMON_NAME,
+            sans_dns=[CERTIFICATE_COMMON_NAME],
+        )
+        self._store_csr(csr)
+        self._certificates.request_certificate_creation(certificate_signing_request=csr)
+
+    def _delete_private_key(self) -> None:
+        """Removes private key from workload."""
+        if not self._private_key_is_stored():
+            return
+        self._container.remove_path(path=f"{CERTS_DIR_PATH}/{PRIVATE_KEY_NAME}")
+        logger.info("Removed private key from workload")
+
+    def _delete_csr(self) -> None:
+        """Deletes CSR from workload."""
+        if not self._csr_is_stored():
+            return
+        self._container.remove_path(path=f"{CERTS_DIR_PATH}/{CSR_NAME}")
+        logger.info("Removed CSR from workload")
+
+    def _delete_certificate(self) -> None:
+        """Deletes certificate from workload."""
+        if not self._certificate_is_stored():
+            return
+        self._container.remove_path(path=f"{CERTS_DIR_PATH}/{CERTIFICATE_NAME}")
+        logger.info("Removed certificate from workload")
+
+    def _private_key_is_stored(self) -> bool:
+        """Returns whether private key is stored in workload."""
+        return self._container.exists(path=f"{CERTS_DIR_PATH}/{PRIVATE_KEY_NAME}")
+
+    def _csr_is_stored(self) -> bool:
+        """Returns whether CSR is stored in workload."""
+        return self._container.exists(path=f"{CERTS_DIR_PATH}/{CSR_NAME}")
+
+    def _get_stored_certificate(self) -> str:
+        """Returns stored certificate."""
+        return str(self._container.pull(path=f"{CERTS_DIR_PATH}/{CERTIFICATE_NAME}").read())
+
+    def _get_stored_csr(self) -> str:
+        """Returns stored CSR."""
+        return str(self._container.pull(path=f"{CERTS_DIR_PATH}/{CSR_NAME}").read())
+
+    def _get_stored_private_key(self) -> bytes:
+        """Returns stored private key."""
+        return str(
+            self._container.pull(path=f"{CERTS_DIR_PATH}/{PRIVATE_KEY_NAME}").read()
+        ).encode()
+
+    def _certificate_is_stored(self) -> bool:
+        """Returns whether certificate is stored in workload."""
+        return self._container.exists(path=f"{CERTS_DIR_PATH}/{CERTIFICATE_NAME}")
+
+    def _store_certificate(self, certificate: str) -> None:
+        """Stores certificate in workload."""
+        self._container.push(path=f"{CERTS_DIR_PATH}/{CERTIFICATE_NAME}", source=certificate)
+        logger.info("Pushed certificate pushed to workload")
+
+    def _store_private_key(self, private_key: bytes) -> None:
+        """Stores private key in workload."""
+        self._container.push(
+            path=f"{CERTS_DIR_PATH}/{PRIVATE_KEY_NAME}",
+            source=private_key.decode(),
+        )
+        logger.info("Pushed private key to workload")
+
+    def _store_csr(self, csr: bytes) -> None:
+        """Stores CSR in workload."""
+        self._container.push(path=f"{CERTS_DIR_PATH}/{CSR_NAME}", source=csr.decode().strip())
+        logger.info("Pushed CSR to workload")
+
     def _configure_pebble(self, restart: bool = False) -> None:
         """Configure the Pebble layer.
 
@@ -121,6 +279,7 @@ class PCFOperatorCharm(CharmBase):
             nrf_url=self._nrf_requires.nrf_url,
             pcf_sbi_port=PCF_SBI_PORT,
             pod_ip=_get_pod_ip(),  # type: ignore[arg-type]
+            scheme="https" if self._certificate_is_stored() else "http",
         )
         if not self._config_file_is_written() or not self._config_file_content_matches(
             content=content
@@ -137,6 +296,7 @@ class PCFOperatorCharm(CharmBase):
         nrf_url: str,
         pcf_sbi_port: int,
         pod_ip: str,
+        scheme: str,
     ) -> str:
         """Renders the config file content.
 
@@ -144,6 +304,7 @@ class PCFOperatorCharm(CharmBase):
             nrf_url (str): NRF URL.
             pcf_sbi_port (int): PCF SBI port.
             pod_ip (str): Pod IPv4.
+            scheme (str): SBI interface scheme ("http" or "https")
 
         Returns:
             str: Config file content.
@@ -156,6 +317,7 @@ class PCFOperatorCharm(CharmBase):
             nrf_url=nrf_url,
             pcf_sbi_port=pcf_sbi_port,
             pod_ip=pod_ip,
+            scheme=scheme,
         )
 
     def _write_config_file(self, content: str) -> None:
