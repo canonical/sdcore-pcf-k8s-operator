6e86b7cac3f9f861f1db06e2484636baff942436
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2024-02-12 10:51:52 +0100

    Updating app_name output name

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-output "pcf_application_name" {
+output "app_name" {
   description = "Name of the deployed application."
   value       = juju_application.pcf.name
 }
