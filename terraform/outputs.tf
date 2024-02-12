ac54f44b4ae90aa5b7f74caf4b60a188e0ff4955
Bartlomiej Gmerek <42570669+Gmerold@users.noreply.github.com>
2024-02-12 11:19:13 +0100

    chore: Updating Terraform module (#57)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,24 @@
-output "pcf_application_name" {
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+output "app_name" {
   description = "Name of the deployed application."
   value       = juju_application.pcf.name
+}
+
+# Required integration endpoints
+
+output "fiveg_nrf_endpoint" {
+  description = "Name of the endpoint used to integrate with the NRF."
+  value = "fiveg-nrf"
+}
+
+output "database_endpoint" {
+  description = "Name of the endpoint used to integrate with the database."
+  value = "database"
+}
+
+output "certificates_endpoint" {
+  description = "Name of the endpoint used to integrate with the TLS certificates provider."
+  value = "certificates"
 }
