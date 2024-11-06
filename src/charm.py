e15b87c9738e8f011b2815d7fa87f198ea2e29f5
Dario Faccin <dario.faccin@canonical.com>
2024-11-06 15:41:39 +0100

    fix: refresh Prometheus scrape endpoint after IP change
    Signed-off-by: Dario Faccin <dario.faccin@canonical.com>

--- src/charm.py
+++ src/charm.py
@@ -72,6 +72,7 @@ class PCFOperatorCharm(CharmBase):
         )
         self._pcf_metrics_endpoint = MetricsEndpointProvider(
             self,
+            refresh_event=[self.on.update_status],
             jobs=[
                 {
                     "static_configs": [{"targets": [f"*:{PROMETHEUS_PORT}"]}],
